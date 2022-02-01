44f8e7df0bdc2c006d4cfb76344660b6ac209f55
Riya Patel <riyapatel3114@gmail.com>
2022-02-01 21:22:00 +0530

    Add project files.

---
+++ ImageAdapter.cs
@@ -0,0 +1,55 @@
+﻿using Android.Content;
+using Android.Views;
+using Android.Widget;
+
+
+namespace Galleryview
+{
+    
+
+    public class ImageAdapter : BaseAdapter
+    {
+        Context c;
+
+        public ImageAdapter(Context c)
+        {
+            this.c = c;
+        }
+
+        public override int Count { get { return thumbIds.Length; } }
+
+        public override Java.Lang.Object GetItem(int position)
+        {
+            return null;
+        }
+
+        public override long GetItemId(int position)
+        {
+            return 0;
+        }
+
+        // create a new ImageView for each item referenced by the Adapter
+        [System.Obsolete]
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            ImageView i = new ImageView(c);
+
+            i.SetImageResource(thumbIds[position]);
+            i.LayoutParameters = new Gallery.LayoutParams(650, 750);
+            i.SetScaleType(ImageView.ScaleType.FitXy);
+
+            return i;
+        }
+
+        // references to our images
+        int[] thumbIds = {
+            
+            Resource.Drawable.flow,
+            Resource.Drawable.flowers,
+            Resource.Drawable.Royalty,
+            Resource.Drawable.yellow,
+            Resource.Drawable.sakae
+     };
+    }
+    
+}
---
+++ MainActivity.cs
@@ -0,0 +1,37 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using System;
+
+namespace Galleryview
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        [Obsolete]
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            Gallery gallery = (Gallery)FindViewById<Gallery>(Resource.Id.gallery);
+
+            gallery.Adapter = new ImageAdapter(this);
+
+            gallery.ItemClick += delegate (object sender, Android.Widget.AdapterView.ItemClickEventArgs args) {
+                Toast.MakeText(this, args.Position.ToString(), ToastLength.Short).Show();
+            };
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
